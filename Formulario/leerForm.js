99b33fdc94a6eca24886c118a710250a9d4b4ff4
INFERNUSRT23 <159806073+INFERNUSRT23@users.noreply.github.com>
2025-05-01 23:10:07 -0600

    Add files via upload

---
+++ Formulario/leerForm.js
@@ -0,0 +1,22 @@
+function leer() {
+    // Referencia por pseudoclase
+    var nom=document.forms["formulario"].elements[0].value;
+    // Referencia por Id
+    var clave=document.getElementById("pass").value;
+    // Referencia por TagName
+    var car=document.getElementsByTagName("select")[0].value;
+    // Referencia por Name
+    var gen=document.getElementsByName("genero");
+    for(i=0; i<gen.length; i++) {
+        if(gen[i].checked)
+            var g=gen[i].value;
+    }
+    var ok=document.getElementById("check").checked;
+    document.getElementById("resultado").innerHTML=
+    "\<br>Tu nombre: "+nom+
+    "\<br>Tu password: "+clave+
+    "\<br>Tu carrera: "+car+
+    "\<br>Tu genero: "+g+
+    "\<br>Aceptado: "+ok;
+
+}
